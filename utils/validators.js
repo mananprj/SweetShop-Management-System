a6364981dbbfec8cb6f32253a9daf6a49634a647
Manan Prjapati <mananprajapati1510@gmail.com>
2025-07-16 17:10:25 +0530

    [Updated]: validators file

--- utils/validators.js
+++ utils/validators.js
@@ -2,12 +2,16 @@ function validateName(name) {
   if (!name || typeof name !== "string" || !name.trim()) {
     throw new Error("Invalid or missing sweet name.");
   }
+
+  return name;
 }
 
 function validateAmount(amount) {
   if (!Number.isInteger(amount) || amount <= 0) {
     throw new Error("Price must be a number greater than 0.");
   }
+
+  return amount;
 }
 
 module.exports = { validateName, validateAmount };
