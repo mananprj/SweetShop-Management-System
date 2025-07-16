e19078f52c013da378c4de6724bb3a3379ff3045
Manan Prjapati <mananprajapati1510@gmail.com>
2025-07-16 13:08:26 +0530

    [Test]: for adding sweet

---
+++ test/addSweet.test.js
@@ -0,0 +1,33 @@
+const { SWEETS } = require('../store/SWEETS');
+const { addSweet } = require('../app/addSweet.js');
+
+describe('addSweet', () => {
+
+    beforeEach(() => {
+        SWEETS.length = 0; // clear array
+    });
+
+
+    test("should generate UUIDs for each sweet", () => {
+        addSweet("Kaju Katli", "Dry Fruit", 500, 20);
+        addSweet("Gulab Jamun", "Fried", 300, 15);
+
+        const ids = SWEETS.map(sweet => sweet.id);
+
+        expect(ids.length).toBe(2);
+        expect(new Set(ids).size).toBe(2); // ensure unique IDs
+    });
+
+    test("should add sweet to SWEETS array", () => {
+        addSweet("Kaju Katli", "Dry Fruit", 500, 20);
+
+        expect(SWEETS.length).toBe(1);
+        expect(SWEETS[0].name).toBe("Kaju Katli");
+    });
+
+    test("should not add sweet if validation fails", () => {
+        addSweet("", "Dry Fruit", 500, 20);
+
+        expect(SWEETS.length).toBe(0);
+    });
+});
