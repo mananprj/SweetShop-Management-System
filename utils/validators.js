cb594d80ef9bb302c7d23e5561ac9f3e80a26555
Manan Prjapati <mananprajapati1510@gmail.com>
2025-07-16 22:10:58 +0530

    [Updated]: Add new functions for validation

--- utils/validators.js
+++ utils/validators.js
@@ -4,16 +4,24 @@ function validateName(name) {
   if (!name || typeof name !== "string" || !name.trim()) {
     throw new Error("Invalid or missing sweet name.");
   }
+}
 
-  return name;
+function validateCategory(category) {
+  if (!category || typeof category !== "string" || !category.trim()) {
+    throw new Error("Invalid or missing sweet category.");
+  }
 }
 
 function validateAmount(amount) {
   if (!Number.isInteger(amount) || amount <= 0) {
     throw new Error("Amount must be a Positive integer and greater than zero.");
   }
+}
 
-  return amount;
+function validateQuantity(quantity) {
+  if (!Number.isInteger(quantity) || quantity <= 0) {
+    throw new Error("Quantity must be a Positive integer and greater than zero.");
+  }
 }
 
 function searchForSweet(name) {
@@ -32,4 +40,4 @@ function checkSweetQuantity(name, quantity) {
   }
 }
 
-module.exports = { validateName, validateAmount, searchForSweet, checkSweetQuantity };
+module.exports = { validateName, validateAmount, searchForSweet, checkSweetQuantity, validateCategory, validateQuantity };
