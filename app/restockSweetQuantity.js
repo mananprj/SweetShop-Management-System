71d5db6a5be66d6ab6640e0408d5b088466693d6
Manan Prjapati <mananprajapati1510@gmail.com>
2025-07-17 10:55:23 +0530

    [Updated]: Function for restock sweet quantity

--- app/restockSweetQuantity.js
+++ app/restockSweetQuantity.js
@@ -1,4 +1,4 @@
-const { validateAmount, validateName } = require('../utils/validators.js');
+const { validateAmount, validateName, searchForSweet } = require('../utils/validators.js');
 const { SWEETS } = require('../store/SWEETS.js');
 
 function restockSweetQuantity(name, amount){
@@ -6,11 +6,7 @@ function restockSweetQuantity(name, amount){
     validateAmount(amount);
     validateName(name);
 
-    const sweet = SWEETS.find(s => s.name.toLowerCase() === name.toLowerCase());
-
-    if(!sweet){
-        throw new Error(`Sweet with name "${name}" not found.`);
-    }
+    const sweet = searchForSweet(name);
 
     sweet.quantity += amount;
 }
