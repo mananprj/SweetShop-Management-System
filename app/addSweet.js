0cbe8dbca1f246825e522852cb7af566ca773716
Manan Prjapati <mananprajapati1510@gmail.com>
2025-07-22 18:54:13 +0530

    [Updated]: Function for adding sweets

--- app/addSweet.js
+++ app/addSweet.js
@@ -1,12 +1,12 @@
 const { v4: uuidv4 } = require('uuid');
 // const { validateSweet } = require('../utils/validateSweet.js');
 const { SWEETS } = require('../store/SWEETS.js');
-const { validateName, validateCategory, validateAmount, validateQuantity } = require('../utils/validators.js');
+const { validateName, validateCategory, validateAmount, validateQuantity, validatekeyword } = require('../utils/validators.js');
 
 function addSweet(name, category, price, quantity){
     
     validateName(name);
-    validateCategory(category);
+    validatekeyword(category);
     validateAmount(price);
     validateQuantity(quantity);
 
